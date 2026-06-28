e11fdead050c7481cd488b1b94dacc1e1120f879
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-06-28 23:09:03 +0200

    refreactor: add more technologies

--- src/slides/TechSlide.jsx
+++ src/slides/TechSlide.jsx
@@ -2,39 +2,124 @@ export default function TechSlide() {
   return (
     <div className="slide" id="slide-tech">
       <div className="content">
-        <h1>Technologies I use</h1>
+        <h1>My tech stack</h1>
+        <div class="tech">
+          <br /><br />
+          <h2>Programming languages:</h2>
+          <div className="block">
+            <span>
+              <i className="devicon-java-plain xl" />
+              <br />
+              <strong>Java</strong>
+            </span>
+            <span>
+              <i className="devicon-cplusplus-plain xl" />
+              <br />
+              <strong>C/C++</strong>
+            </span>
+            <span>
+              <i className="devicon-csharp-plain xl" />
+              <br />
+              <strong>C#</strong>
+            </span>
+            <span>
+              <i className="devicon-python-plain xl" />
+              <br />
+              <strong>Python</strong>
+            </span>
+            <span>
+              <i className="devicon-typescript-plain xl" />
+              <br />
+              <strong>TypeScript</strong>
+            </span>
+            <span>
+              <i className="devicon-javascript-plain xl" />
+              <br />
+              <strong>JavaScript</strong>
+            </span>
+          </div>
 
-        <div className="block">
-          <span>
-            Mainly:
-            <br /><br />
-            <i className="fa-brands fa-java xl" />
-            <br />
-            <strong>Java</strong>
-          </span>
-        </div>
+          <h2>Frameworks:</h2>
+          <div className="block">
+            <span>
+              <i className="devicon-spring-plain xl" />
+              <br />
+              <strong>Spring Boot</strong>
+            </span>
+            <span>
+              <i className="devicon-dotnetcore-plain xl" />
+              <br />
+              <strong>.NET</strong>
+            </span>
+          </div>
 
-        <div className="block">
-          <span>
-            <i className="devicon-cplusplus-plain xl" />
-            <br /><br />
-            <strong>C/C++</strong>
-          </span>
-          <span>
-            <i className="fa-brands fa-python xl" />
-            <br />
-            <strong>Python</strong>
-          </span>
-          <span>
-            <i className="devicon-javascript-plain xl" />
-            <br /><br />
-            <strong>JavaScript</strong>
-          </span>
-          <span>
-            <i className="devicon-php-plain xl" />
-            <br /><br />
-            <strong>PHP</strong>
-          </span>
+          <h2>Frontend:</h2>
+          <div className="block">
+            <span>
+              <i className="devicon-angularjs-plain xl" />
+              <br />
+              <strong>Angular</strong>
+            </span>
+            <span>
+              <i className="devicon-react-original xl" />
+              <br />
+              <strong>React</strong>
+            </span>
+            <span>
+              <i className="devicon-html5-plain xl" />
+              <br />
+              <strong>HTML</strong>
+            </span>
+            <span>
+              <i className="devicon-css3-plain xl" />
+              <br />
+              <strong>CSS</strong>
+            </span>
+          </div>
+
+          <h2>Databases:</h2>
+          <div className="block">
+            <span>
+              <i className="devicon-postgresql-plain xl" />
+              <br />
+              <strong>PostgreSQL</strong>
+            </span>
+            <span>
+              <i className="devicon-mongodb-plain xl" />
+              <br />
+              <strong>MongoDB</strong>
+            </span>
+            <span>
+              <i className="devicon-mysql-plain xl" />
+              <br />
+              <strong>MySQL</strong>
+            </span>
+            <span>
+              <i className="devicon-sqlite-plain xl" />
+              <br />
+              <strong>SQLite</strong>
+            </span>
+          </div>
+
+          <h2>DevOps:</h2>
+          <div className="block">
+            <span>
+              <i className="devicon-gradle-plain xl" />
+              <br />
+              <strong>Gradle</strong>
+            </span>
+            <span>
+              <i className="devicon-docker-plain xl" />
+              <br />
+              <strong>Docker</strong>
+            </span>
+            <span>
+              <i className="devicon-kubernetes-plain xl" />
+              <br />
+              <strong>Kubernates</strong>
+            </span>
+          </div>
+
         </div>
       </div>
     </div>
