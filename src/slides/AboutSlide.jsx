405b69c85c01a03b7f44766a3d646f9a4b1892b2
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-06-28 23:08:33 +0200

    refreactor: change the 'About me' section description

--- src/slides/AboutSlide.jsx
+++ src/slides/AboutSlide.jsx
@@ -1,5 +1,5 @@
 export default function AboutSlide() {
-  const birthDate = new Date(2002, 7, 7);
+  const birthDate = new Date(2002, 8, 7);
   const ageDiff = new Date().getTime() - birthDate.getTime();
   const age = Math.floor(ageDiff / (1000 * 60 * 60 * 24 * 365.25));
 
@@ -12,17 +12,17 @@ export default function AboutSlide() {
             <br />
             <strong>
               My name is Łukasz (eng. <b>Luke</b>) alias <b>Wardiusz</b>. I&apos;m from <b>Poland</b>. Age <b>{age}</b>.{' '}
-              <b>Passionate programmer</b> who wants to make useful, enjoyable stuff.{' '}
+              <b>Passionate programmer</b> who wants to make useful, secure and enjoyable stuff.{' '}
               Student of{' '}
               <a href="https://pja.edu.pl/en/" target="_blank" rel="noreferrer"><b>Polish-Japanese Academy of Information Technology</b></a>{' '}
               in short{' '}
               <a href="https://pja.edu.pl/en/" target="_blank" rel="noreferrer"><b>PJAIT</b></a>.{' '}
-              Dreamer who <b>wants to be one of the best</b> in the field.{' '}
-              Person who&apos;s <b>devoted to his work</b>.{' '}
+              Aspiring <b>Fullstack Developer</b>.{' '}
               <b>Open minded</b> on ever changing world and it&apos;s people.{' '}
-              <b>Fast learner</b> who tries to learn multiple programming languages.{' '}
-              Big weeb/weeaboo, musician, gamer, car/motorcycle/gun enthusiast, martial arts enjoyer and of course programmer.{' '}
-              <b>All of it describes me best</b>.
+              <b>Fast learner</b> who tries to learn new things everyday.{' '}
+              Big weeb, musician, gamer, car, motorcycle and gun enthusiast and of course programmer.{' '}
+              <b>All of it describes me best</b>.{' '}
+              <i>I use arch btw.</i>
             </strong>
           </span>
         </div>
